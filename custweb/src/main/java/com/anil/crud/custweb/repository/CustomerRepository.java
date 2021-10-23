6aa9041cbfe4755232722aa8e4842b0475dba973
Anil <apratapani@gmail.com>
2021-10-23 05:21:55 -0500

    removed custweb

--- custweb/src/main/java/com/anil/crud/custweb/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.anil.crud.custweb.repository;
-
-import com.anil.crud.custweb.domain.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
-
-}
